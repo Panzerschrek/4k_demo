cfae43f45f7d83fa414445f9bf3f98ff8b4d9f94
Panzerschrek <AntiPanzerschrek@yandex.ru>
2024-07-15 13:14:27 +0300

    Minimize size by merging more sections and using our own DOS stub.

--- main.cpp
+++ main.cpp
@@ -3,6 +3,6 @@
 int main()
 {
 	DWORD written = 0;
-	const char message[] = "hello world";
+	static const char message[] = "hello world";
 	WriteConsoleA(GetStdHandle(STD_OUTPUT_HANDLE), message, DWORD(sizeof(message) - 1), &written, NULL);
 }
