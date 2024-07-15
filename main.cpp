add6f3ad79b83e8d4994359a6f594789079f83f7
Panzerschrek <AntiPanzerschrek@yandex.ru>
2024-07-15 12:52:16 +0300

    Reduce executable size fast up to 1kb.

--- main.cpp
+++ main.cpp
@@ -2,5 +2,7 @@
 
 int main()
 {
-	OutputDebugStringW(L"Hello, world!\n");
+	DWORD written = 0;
+	const char message[] = "hello world";
+	WriteConsoleA(GetStdHandle(STD_OUTPUT_HANDLE), message, DWORD(sizeof(message) - 1), &written, NULL);
 }
