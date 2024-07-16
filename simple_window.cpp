6141c5f34fa534ad75ff5390b2fd7d25967dc7d5
Panzerschrek <AntiPanzerschrek@yandex.ru>
2024-07-16 10:22:50 +0300

    Move window-related code into a separate class.

--- simple_window.cpp
+++ simple_window.cpp
@@ -1,114 +1,23 @@
-#include <cstdint>
-#include <windows.h>
-
-
-static LRESULT CALLBACK WindowProc(const HWND hwnd, const UINT msg, const WPARAM w_param, const LPARAM l_param)
-{
-	switch(msg)
-	{
-	// Do not bothre quiting via "main" function - just exit process.
-	// TODO - find more compact way to quit application.
-	case WM_CLOSE:
-	case WM_QUIT:
-		ExitProcess(0);
-		break;
-	}
-
-	return DefWindowProcA(hwnd, msg, w_param, l_param);
-}
-
-static const char* const g_window_class = "4k_demo";
-static const char* const g_window_name = "4k_demo";
+#include "window.hpp"
 
 int main()
 {
-	// Create window class.
+	DrawableWindow window("4k_demo", 640, 480);
 
-	WNDCLASSEXA window_class{};
-	window_class.cbSize = sizeof(WNDCLASSEX);
-	window_class.style = CS_OWNDC;
-	window_class.lpfnWndProc = WindowProc;
-	window_class.cbClsExtra = 0;
-	window_class.cbWndExtra = 0;
-	window_class.hInstance = 0;
-	window_class.hIcon = nullptr; // Use default icon.
-	window_class.hCursor = LoadCursorA(nullptr, IDC_ARROW);
-	window_class.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
-	window_class.lpszMenuName = nullptr;
-	window_class.lpszClassName = g_window_class;
-	window_class.hIconSm = nullptr; // Use default icon.
-
-	RegisterClassExA(&window_class);
-
-	// Create window itself.
-
-	const int window_width = 640;
-	const int window_height = 480;
-
-	const HWND hwnd =
-		CreateWindowExA(
-			0,
-			g_window_class,
-			g_window_name,
-			WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_BORDER,
-			0, 0,
-			window_width, window_height,
-			nullptr,
-			nullptr,
-			0,
-			nullptr);
-
-	// Create stuff for drawing.
-
-	const HDC hdc = GetDC(hwnd);
-
-	BITMAPINFO bitmap_info{};
-	bitmap_info.bmiHeader.biSize = sizeof(BITMAPINFOHEADER);
-	bitmap_info.bmiHeader.biWidth = window_width;
-	bitmap_info.bmiHeader.biHeight = -window_height;
-	bitmap_info.bmiHeader.biSizeImage = -(window_height * window_width);
-	bitmap_info.bmiHeader.biPlanes = 1;
-	bitmap_info.bmiHeader.biBitCount = 32;
-	bitmap_info.bmiHeader.biCompression = BI_RGB;
-
-	const HDC compatible_dc = CreateCompatibleDC(hdc);
-
-	using PixelType = uint32_t;
-
-	PixelType* pixels = nullptr;
-
-	const HBITMAP bitmap_handle =
-		CreateDIBSection(hdc, &bitmap_info, DIB_RGB_COLORS, reinterpret_cast<void**>(&pixels), nullptr, 0);
-
-	SelectObject(compatible_dc, bitmap_handle);
-
-	// Show the window.
-
-	ShowWindow(hwnd, SW_SHOWNORMAL);
-
-	// Main loop.
-
-	int iteration = 0;
+	uint32_t iteration = 0;
 
 	while(true)
 	{
-		MSG message;
-		while(PeekMessageA(&message, NULL, 0, 0, PM_REMOVE) != 0)
-		{
-			TranslateMessage(&message);
-			DispatchMessageA(&message);
-		}
+		window.ProcessMessages();
 
-		for(uint32_t y= 0; y < uint32_t(window_height); ++y)
-			for(uint32_t x = 0; x < uint32_t(window_width); ++x)
-				pixels[x + y * uint32_t(window_width)] = (x >> 2) | ((y >> 1) << 8) | (((iteration << 1) & 255) << 16);
+		for(uint32_t y= 0; y < window.GetHeight(); ++y)
+			for(uint32_t x = 0; x < window.GetWidth(); ++x)
+				window.GetPixels()[x + y * window.GetWidth()] = (x >> 2) | ((y >> 1) << 8) | (((iteration << 1) & 255) << 16);
 
-		BitBlt(hdc, 0, 0, window_width, window_height, compatible_dc, 0, 0, SRCCOPY);
+		window.Blit();
 
 		Sleep(16);
 
 		++iteration;
 	}
-
-	// Save some space - do not bother with deinitialization/clearing.
 }
---
+++ window.hpp
@@ -0,0 +1,131 @@
+#pragma once
+#include <cstdint>
+#include <Windows.h>
+
+class DrawableWindow
+{
+public:
+	using PixelType = uint32_t;
+
+public:
+	// All methods are inline - for further inlining and avoiding of unnecessary calls.
+
+	DrawableWindow(const char* const title, const uint32_t width, const uint32_t height)
+		: width_(width), height_(height)
+	{
+		const char* const window_class_name = "4k_demo";
+
+		// Create window class.
+
+		WNDCLASSEXA window_class{};
+		window_class.cbSize = sizeof(WNDCLASSEX);
+		window_class.style = CS_OWNDC;
+		window_class.lpfnWndProc = WindowProc;
+		window_class.cbClsExtra = 0;
+		window_class.cbWndExtra = 0;
+		window_class.hInstance = 0;
+		window_class.hIcon = nullptr; // Use default icon.
+		window_class.hCursor = LoadCursorA(nullptr, IDC_ARROW);
+		window_class.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
+		window_class.lpszMenuName = nullptr;
+		window_class.lpszClassName = window_class_name;
+		window_class.hIconSm = nullptr; // Use default icon.
+
+		RegisterClassExA(&window_class);
+
+		hwnd_ =
+			CreateWindowExA(
+				0,
+				window_class_name,
+				title,
+				WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_BORDER,
+				0, 0,
+				int(width_), int(height_),
+				nullptr,
+				nullptr,
+				0,
+				nullptr);
+
+		// Create stuff for drawing.
+
+		hdc_ = GetDC(hwnd_);
+
+		// Create bitmap for the whole screen size with 32-bit color.
+		BITMAPINFO bitmap_info{};
+		bitmap_info.bmiHeader.biSize = sizeof(BITMAPINFOHEADER);
+		bitmap_info.bmiHeader.biWidth = int(width_);
+		bitmap_info.bmiHeader.biHeight = -int(height_);
+		bitmap_info.bmiHeader.biSizeImage = -int(height_ * width_);
+		bitmap_info.bmiHeader.biPlanes = 1;
+		bitmap_info.bmiHeader.biBitCount = 32;
+		bitmap_info.bmiHeader.biCompression = BI_RGB;
+
+		compatible_dc_ = CreateCompatibleDC(hdc_);
+
+		const HBITMAP bitmap_handle =
+			CreateDIBSection(hdc_, &bitmap_info, DIB_RGB_COLORS, reinterpret_cast<void**>(&pixels_), nullptr, 0);
+
+		SelectObject(compatible_dc_, bitmap_handle);
+
+		// Show the window.
+
+		ShowWindow(hwnd_, SW_SHOWNORMAL);
+	}
+
+	// Save some executable space - do not bother with deinitialization/clearing.
+	~DrawableWindow() = default;
+
+	PixelType* GetPixels() const
+	{
+		return pixels_;
+	}
+
+	uint32_t GetWidth() const
+	{
+		return width_;
+	}
+
+	uint32_t GetHeight() const
+	{
+		return height_;
+	}
+
+	void ProcessMessages()
+	{
+		MSG message;
+		while (PeekMessageA(&message, NULL, 0, 0, PM_REMOVE) != 0)
+		{
+			TranslateMessage(&message);
+			DispatchMessageA(&message);
+		}
+	}
+
+	void Blit()
+	{
+		BitBlt(hdc_, 0, 0, int(width_), int(height_), compatible_dc_, 0, 0, SRCCOPY);
+	}
+
+private:
+	static LRESULT CALLBACK WindowProc(const HWND hwnd, const UINT msg, const WPARAM w_param, const LPARAM l_param)
+	{
+		switch (msg)
+		{
+			// Do not bothre quiting via "main" function - just exit process.
+			// TODO - find more compact way to quit application.
+		case WM_CLOSE:
+		case WM_QUIT:
+			ExitProcess(0);
+			break;
+		}
+
+		return DefWindowProcA(hwnd, msg, w_param, l_param);
+	}
+
+private:
+	const uint32_t width_;
+	const uint32_t height_;
+	HWND hwnd_;
+	HDC hdc_;
+	HDC compatible_dc_;
+	PixelType* pixels_;
+};
