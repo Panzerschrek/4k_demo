77abe8bc126945724bd4615e41f1df92ae8bfa2a
Panzerschrek <AntiPanzerschrek@yandex.ru>
2024-07-26 11:19:07 +0300

    Some style changes.

--- sound_out.cpp
+++ sound_out.cpp
@@ -11,57 +11,56 @@ int main()
 
 	uint32_t iteration = 0;
 
-	HWAVEOUT hWaveOut = 0;
-	WAVEFORMATEX wfx = { WAVE_FORMAT_PCM, 1, 8000, 8000, 1, 8, 0 };
-	waveOutOpen(&hWaveOut, WAVE_MAPPER, &wfx, 0, 0, CALLBACK_NULL);
+	HWAVEOUT wave_out = 0;
+	WAVEFORMATEX wfx{ WAVE_FORMAT_PCM, 1, 8000, 8000, 1, 8, 0 };
+	waveOutOpen(&wave_out, WAVE_MAPPER, &wfx, 0, 0, CALLBACK_NULL);
 
 	// Make this size at least a good fraction of the sample rate.
-	const DWORD chunkSize = 500;
-	// It is good to have at least two buffers. One buffer causes crackles in the sound output.
-	const DWORD numHeaders = 2;
-	WAVEHDR headers[numHeaders];
-	char bufferData[numHeaders][chunkSize];
+	constexpr uint32_t chunk_size = 512;
+	// Use double buffering.
+	constexpr uint32_t num_headers = 2;
+	WAVEHDR headers[num_headers];
+	char buffer_data[num_headers][chunk_size];
 
-	// Setup all buffers to be "done" so they are filled the first time around
-	for (int i = 0; i < numHeaders; i++)
-	{
+	// Setup all buffers to be "done" so they are filled the first time around.
+	for(uint32_t i = 0; i < num_headers; ++i)
 		headers[i].dwFlags = WHDR_DONE;
-	}
 
-	DWORD blockIndex = 0;
+	uint32_t block_index = 0;
 	// This demonstrates sending data as chunks into the output headers
 	// Note no callback is needed because we test the dwFlags for WHDR_DONE
 	DWORD t = 0;
-	while (true)
+	while(true)
 	{
-		WAVEHDR* header = headers + blockIndex;
+		WAVEHDR* const header = headers + block_index;
 		// Fill any buffers that are "done"
-		if (header->dwFlags & WHDR_DONE)
+		if((header->dwFlags & WHDR_DONE) != 0)
 		{
 			header->dwFlags = 0;
-			header->lpData = bufferData[blockIndex];
+			header->lpData = buffer_data[block_index];
 			// Calculate a new chunk of data
-			for (int i = 0; i < chunkSize; i++)
+			for(uint32_t i = 0; i < chunk_size; ++i)
 			{
 				// See http://goo.gl/hQdTi
-				bufferData[blockIndex][i] = static_cast<char>((((t * (t >> 8 | t >> 9) & 46 & t >> 8)) ^ (t & t >> 13 | t >> 6)) & 0xFF);
+				buffer_data[block_index][i] = static_cast<char>((((t * (t >> 8 | t >> 9) & 46 & t >> 8)) ^ (t & t >> 13 | t >> 6)) & 0xFF);
 				t++;
 			}
-			header->dwBufferLength = chunkSize;
-			waveOutPrepareHeader(hWaveOut, header, sizeof(WAVEHDR));
-			waveOutWrite(hWaveOut, header, sizeof(WAVEHDR));
-			waveOutUnprepareHeader(hWaveOut, header, sizeof(WAVEHDR));
+			header->dwBufferLength = chunk_size;
+			waveOutPrepareHeader(wave_out, header, sizeof(WAVEHDR));
+			waveOutWrite(wave_out, header, sizeof(WAVEHDR));
+			waveOutUnprepareHeader(wave_out, header, sizeof(WAVEHDR));
 		}
 
 		// Test the next buffer
-		blockIndex++;
-		if (blockIndex == numHeaders)
+		++block_index;
+		if(block_index == num_headers)
 		{
 			// If we have tested all buffers, then sleep for a very short while to avoid blocking the whole thread
-			blockIndex = 0;
+			block_index = 0;
 			Sleep(10);
 		}
 
+		// Draw.
 		window.ProcessMessages();
 
 		for (uint32_t y = 0; y < window.GetHeight(); ++y)
@@ -72,9 +71,7 @@ int main()
 				(((iteration << 1) & 255) << 16);
 
 		window.Blit();
-
-		Sleep(16);
 	}
 
-	waveOutClose(hWaveOut);
+	waveOutClose(wave_out);
 }
