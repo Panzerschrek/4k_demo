1cbbd7a42af2e4da80bb939bec4cdfc6d87bf853
Panzerschrek <AntiPanzerschrek@yandex.ru>
2024-07-26 13:44:25 +0300

    Some code simplifications.

--- orbital_resonance.cpp
+++ orbital_resonance.cpp
@@ -98,7 +98,6 @@ int main()
 	auto& buffer_data = *reinterpret_cast<BufferData*>(VirtualAlloc(nullptr, sizeof(BufferData), MEM_COMMIT, PAGE_READWRITE));
 
 	HWAVEOUT wave_out = 0;
-
 	waveOutOpen(&wave_out, WAVE_MAPPER, &wfx, 0, 0, CALLBACK_NULL);
 
 	WAVEHDR headers[num_headers];
@@ -108,104 +107,105 @@ int main()
 		headers[i].dwBufferLength = chunk_size * sizeof(SampleType);
 	}
 
-	const uint32_t screen_area = window.GetWidth() * window.GetHeight();
-
 	uint32_t block_index_linear = 0;
 	uint32_t t = 0;
 	while(true)
 	{
 		const uint32_t block_index = block_index_linear % num_headers;
 		WAVEHDR& header = headers[block_index];
-		// Fill any buffers that are "done"
-		if((header.dwFlags & WHDR_DONE) != 0)
+
+		if((header.dwFlags & WHDR_DONE) == 0)
 		{
-			// Clear previous usage.
-			waveOutUnprepareHeader(wave_out, &header, sizeof(WAVEHDR));
+			// Not done yet processing next buffer. Wait a bit.
+			Sleep(1);
+			continue;
+		}
+		
+		// Clear previous buffer usage.
+		waveOutUnprepareHeader(wave_out, &header, sizeof(WAVEHDR));
 
-			header.dwFlags = 0;
-			header.lpData = reinterpret_cast<LPSTR>(buffer_data[block_index]);
+		header.dwFlags = 0;
+		header.lpData = reinterpret_cast<LPSTR>(buffer_data[block_index]);
 
-			// Calculate a new chunk of data.
-			for(uint32_t i = 0; i < chunk_size; ++i)
-			{
-				const float w = float(t) * (c_base_freq / float(sampling_frequency));
-				const float p = w * Math::tau;
+		// Calculate a new chunk of data.
+		for(uint32_t i = 0; i < chunk_size; ++i)
+		{
+			const float w = float(t) * (c_base_freq / float(sampling_frequency));
+			const float p = w * Math::tau;
 
-				float result = 0.0f;
+			float result = 0.0f;
 
-				// See https://www.youtube.com/watch?v=Qyn64b4LNJ0.
-				for (int32_t beat_n = c_start_beat; beat_n <= c_end_beat; ++beat_n)
-				{
-					const float current_period = c_beat_period * float(beat_n);
-					const float current_beat_w = w / current_period;
-					const float beat_factor = current_beat_w - float(int32_t(current_beat_w));
-					if (beat_factor < c_beat_fraction)
-					{
-						float beat_p = p * float(beat_n);
-						result += Math::Sin(beat_p) + Math::Sin(beat_p * 2.0f) * 0.5f + Math::Sin(beat_p * 3.0f) * 0.3333f;
-					}
-				}
-
-				constexpr float amplitude = float(std::numeric_limits<SampleType>::max()) / 16.0f; // Not too loud.
-				buffer_data[block_index][i] = SampleType(int32_t(result * amplitude));
-
-				++t;
-			}
-			
-			waveOutPrepareHeader(wave_out, &header, sizeof(WAVEHDR));
-			waveOutWrite(wave_out, &header, sizeof(WAVEHDR));
-
-			++block_index_linear;
-
-			// Process messages and update window only after successfull sound output.
-	
-			window.ProcessMessages();
-
-			// Draw.
-			ZeroMemoryInline(window.GetPixels(), window.GetWidth() * window.GetHeight() * sizeof(DrawableWindow::PixelType));
-
-			const uint32_t center_x = window.GetWidth() / 2u;
-			const uint32_t center_y = window.GetHeight() / 2u;
-
-			// Alignment line.
-			// A note is emitted when a planet crosses it.
-			constexpr DrawableWindow::PixelType lines_color = 0x00202020;
-			for(uint32_t y = center_y; y < window.GetHeight(); ++y)
-				window.GetPixels()[center_x + y * window.GetWidth()] = lines_color;
-
-			// Sun.
-			FillCircle(window, center_x, center_y, 24, 0x00FFFF40);
-
+			// See https://www.youtube.com/watch?v=Qyn64b4LNJ0.
 			for(int32_t beat_n = c_start_beat; beat_n <= c_end_beat; ++beat_n)
 			{
-				const float orbit_radius = float(beat_n) * 33.0f;
-				const int32_t planet_radius = 44u / uint32_t(3 + c_end_beat - beat_n);
-
-				const float phase = float(t) * (Math::tau / float(sampling_frequency) * c_base_freq / c_beat_period) / float(beat_n);
-				const int32_t dx = int32_t(Math::Sin(phase) * orbit_radius);
-				const int32_t dy = int32_t(Math::Cos(phase) * orbit_radius);
-
-				// Orbit circle.
-				DrawCircle(window, center_x, center_y, int32_t(orbit_radius), lines_color);
-
-				static constexpr DrawableWindow::PixelType colors[c_end_beat - c_start_beat + 1]
+				const float current_period = c_beat_period * float(beat_n);
+				const float current_beat_w = w / current_period;
+				const float beat_factor = current_beat_w - float(int32_t(current_beat_w));
+				if(beat_factor < c_beat_fraction)
 				{
-					0x00D02020,
-					0x00D08020,
-					0x00E0E020,
-					0x0020D020,
-					0x0020D0D0,
-					0x002020D0,
-					0x006040D0,
-					0x00D020D0,
-				};
-
-				FillCircle(window, int32_t(center_x) + dx, int32_t(center_y) + dy, planet_radius, colors[beat_n - c_start_beat]);
+					const float beat_p = p * float(beat_n);
+					result += Math::Sin(beat_p) + Math::Sin(beat_p * 2.0f) * 0.5f + Math::Sin(beat_p * 3.0f) * 0.3333f;
+				}
 			}
 
-			window.Blit();
+			constexpr float amplitude = float(std::numeric_limits<SampleType>::max()) / 16.0f; // Not too loud.
+			buffer_data[block_index][i] = SampleType(int32_t(result * amplitude));
+
+			++t;
 		}
-		else
-			Sleep(1);
+			
+		waveOutPrepareHeader(wave_out, &header, sizeof(WAVEHDR));
+		waveOutWrite(wave_out, &header, sizeof(WAVEHDR));
+
+		++block_index_linear;
+
+		// Process messages and update window only after successfull sound output.
+	
+		window.ProcessMessages();
+
+		// Draw.
+		ZeroMemoryInline(window.GetPixels(), window.GetWidth() * window.GetHeight() * sizeof(DrawableWindow::PixelType));
+
+		const uint32_t center_x = window.GetWidth() / 2u;
+		const uint32_t center_y = window.GetHeight() / 2u;
+
+		// Alignment line.
+		// A note is emitted when a planet crosses it.
+		constexpr DrawableWindow::PixelType lines_color = 0x00202020;
+		for(uint32_t y = center_y; y < window.GetHeight(); ++y)
+			window.GetPixels()[center_x + y * window.GetWidth()] = lines_color;
+
+		// Sun.
+		FillCircle(window, center_x, center_y, 24, 0x00FFFF40);
+
+		// Planets.
+		for(int32_t beat_n = c_start_beat; beat_n <= c_end_beat; ++beat_n)
+		{
+			const int32_t orbit_radius = beat_n * 33;
+			const int32_t planet_radius = 44u / uint32_t(3 + c_end_beat - beat_n);
+
+			const float phase = float(t) * (Math::tau * c_base_freq / (float(sampling_frequency) * c_beat_period)) / float(beat_n);
+			const int32_t dx = int32_t(Math::Sin(phase) * float(orbit_radius));
+			const int32_t dy = int32_t(Math::Cos(phase) * float(orbit_radius));
+
+			// Orbit circle.
+			DrawCircle(window, center_x, center_y, orbit_radius, lines_color);
+
+			static constexpr DrawableWindow::PixelType colors[c_end_beat - c_start_beat + 1]
+			{
+				0x00D02020,
+				0x00D08020,
+				0x00E0E020,
+				0x0020D020,
+				0x0020D0D0,
+				0x002020D0,
+				0x006040D0,
+				0x00D020D0,
+			};
+
+			FillCircle(window, int32_t(center_x) + dx, int32_t(center_y) + dy, planet_radius, colors[beat_n - c_start_beat]);
+		}
+
+		window.Blit();
 	}
 }
