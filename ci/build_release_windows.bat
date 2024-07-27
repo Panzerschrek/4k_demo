913fb3da134c7715080a3c7e238c30ce943b255c
Panzerschrek <AntiPanzerschrek@yandex.ru>
2024-07-27 10:41:29 +0300

    Add CI files for Windows build.

---
+++ ci/build_release_windows.bat
@@ -0,0 +1,4 @@
+"C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsall.bat"
+mkdir build_dir
+cmake -S source -B build_dir -G "Ninja" -DCMAKE_BUILD_TYPE=MinSizeRel
+cmake --build build_dir
