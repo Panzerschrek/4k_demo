22dc23ff145f0deec6c2a0fa66927f4ce0474953
Panzerschrek <AntiPanzerschrek@yandex.ru>
2025-07-02 20:58:17 +0300

    Sobirajem "CI" sborki na "Windows 2022" s ispoljzovanijem "Visual Studio 2022".

--- ci/build_release_windows_32.bat
+++ ci/build_release_windows_32.bat
@@ -1,4 +1,4 @@
-call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" amd64_x86
+call "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" amd64_x86
 mkdir build_dir
 cmake -S . -B build_dir -G "Ninja" -DCMAKE_BUILD_TYPE=MinSizeRel
 cmake --build build_dir
